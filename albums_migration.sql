bdb218e10ebd2f639ac6a2e2e0709267d497761a
megansimmons <simmons.megan584@gmail.com>
2019-04-08 18:18:04 -0500

    albums_migration.sql complete with some walkthru notes

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(50) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  --release_date INT,
+  release_date DATE,
+  --sales DECIMAL(9,5)  --1234.56789  this is like scientific notation
+  sales float,
+  genre VARCHAR(100),
+  PRIMARY KEY (id)
+);
